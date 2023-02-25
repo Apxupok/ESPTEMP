fcc0129cac5eeb0166d262cb5d4b010f6bab72a0
apxupok <apxupok@gmail.com>
2023-02-25 12:29:15 +0500

    Initial commit

---
+++ app/src/main/java/com/example/esptemp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.esptemp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+    fun onMyButtonClick(view: View?) {
+        // выводим сообщение
+        Toast.makeText(this, "Зачем вы нажали?", Toast.LENGTH_SHORT).show()
+    }
+
+
+}
