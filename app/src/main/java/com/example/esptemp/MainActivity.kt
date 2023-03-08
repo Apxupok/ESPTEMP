d915ddb69fc4791835363ec6113bb1962c9b0d15
apxupok <apxupok@gmail.com>
2023-03-08 20:21:45 +0500

    Clear for master

--- app/src/main/java/com/example/esptemp/MainActivity.kt
+++ app/src/main/java/com/example/esptemp/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.example.esptemp
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.loopj.android.http.*
+import cz.msebera.android.httpclient.Header
+import cz.msebera.android.httpclient.HttpResponse
+import cz.msebera.android.httpclient.impl.client.HttpClients
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView = findViewById<TextView>(R.id.textView8)
+// ...
+
+
 
     }
-    fun onMyButtonClick(view: View?) {
-        // выводим сообщение
-        Toast.makeText(this, "Зачем вы нажали?", Toast.LENGTH_SHORT).show()
-    }
-
 
 }
